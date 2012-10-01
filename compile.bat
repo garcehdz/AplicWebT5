532088dc4edafd6384f449dbf330818bf424762d
Gilberth Arce <gah.chubby@gmail.com>
2012-10-01 14:46:20 -0600

    Commit

---
+++ compile.bat
@@ -0,0 +1,5 @@
+PATH=C:\Program Files\Java\jdk1.6.0_25\bin
+
+javac -classpath lib/servlet-api-2.5.jar;lib/commons-logging-1.1.1.jar;www/WEB-INF/lib/spring-webmvc-3.2.0.M1.jar -d www/WEB-INF/classes src/service/*.java src/domain/*.java src/web/*.java
+
+pause
